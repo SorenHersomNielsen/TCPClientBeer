f3c3bd4ed35e1459ae26dbff4418ef74568a535f
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-04-05 19:35:21 +0200

    Initial commit

---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClientBeer
+{
+    public class Client
+    {
+        public void Start()
+        {
+            Console.WriteLine("press enter");
+            Console.ReadLine();
+            TcpClient socket = new TcpClient("localhost", 4646);
+
+            Stream ns = socket.GetStream();
+
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+            Console.WriteLine("write a message");
+            string line = Console.ReadLine();
+            sw.WriteLine(line);
+            string serveranswer = sr.ReadLine();
+            Console.WriteLine("Server: " + serveranswer);
+            Console.ReadLine();
+
+            ns.Close();
+            socket.Close();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TCPClientBeer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Start();
+        }
+    }
+}
